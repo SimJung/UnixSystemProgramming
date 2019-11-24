1166937f8b1196acf331823ee89b7870bdb23959
Jung Sim <tlawjd99gma@gmail.com>
2019-11-24 23:38:19 +0900

    shell project 3th week complete

--- MyShell/myshell.c
+++ MyShell/myshell.c
@@ -10,9 +10,7 @@
 
 const char *prompt = "myshell> ";
 char* cmdvector[MAX_CMD_ARG];
-char* cmdinvector[MAX_CMD_ARG];
-char* cmdoutvector[MAX_CMD_ARG];
-char* cmdpipevector[MAX_CMD_ARG];
+char* cmdPipeVector[MAX_CMD_ARG];
 char cmdline[BUFSIZ];
 
 void fatal (char *str) {
@@ -74,46 +72,68 @@ static void sigchldHandler(int unused){
 	int status;
 	wait(&status);
 }
-
-int pipeProcess(char *t){
-	int fd[2];
-	pipe(fd);
+void redirec(char *s)
+{
+	char t[BUFSIZ];
+	int len = strlen(s), fd, i;
 	
-	char temp[BUFSIZ];
-	strcpy(temp, t);
-	char *t1 = strtok(temp, "|");
-	char *t2 = strtok(NULL, "\0");
-	char *chk = strchr(t2, '|');
-	char* cmdvec[MAX_CMD_ARG];
-	
-	removeFirst(t1);
-	removeFirst(t2);
-
-	switch(fork()){
-	case 0:
-		dup2(fd[1], 1);
-		close(fd[0]);
-		close(fd[1]);
-		makelist(t1, " \t", cmdvec, MAX_CMD_ARG);
-		execvp(cmdvec[0], cmdvec);
-	default:
-		if(chk != NULL)
-			pipeProcess(t2);
-		else{
-			dup2(fd[0], 0);
-			close(fd[0]);
-			close(fd[1]);
-			makelist(t2, " \t", cmdvec, MAX_CMD_ARG);
-			execvp(cmdvec[0], cmdvec);	
+	for(i=0; i<len; i++){
+		if(s[i] == '>'){
+			strcpy(t, strtok(&s[i+1], " \t"));
+			fd = open(t, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			dup2(fd, 1);
+			close(fd);
+			s[i] = '\0';
+		}
+		if(s[i] == '<'){
+			strcpy(t, strtok(&s[i+1], " \t"));
+			fd = open(t, O_RDONLY, 0644);
+			dup2(fd, 0);
+			close(fd);
+			s[i] = '\0';
 		}
 	}
 }
 
+int pipeProcess(char *t){
+	int ppcnt = 0;
+	int fd[2], pid;
+	ppcnt = makelist(t, "|", cmdPipeVector, MAX_CMD_ARG);
+	int i;
+	for(i=0; i<ppcnt; i++){
+		if(i != ppcnt-1){
+			pipe(fd);
+			switch(fork()){
+				case 0:
+					dup2(fd[1], 1);
+					close(fd[0]);
+					close(fd[1]);
+					redirec(cmdPipeVector[i]);
+					makelist(cmdPipeVector[i], " \t", cmdvector, MAX_CMD_ARG);
+					execvp(cmdvector[0], cmdvector);
+				default:
+					dup2(fd[0], 0);
+					close(fd[0]);
+					close(fd[1]);
+	
+			}
+		}else{
+			redirec(cmdPipeVector[i]);
+			makelist(cmdPipeVector[i], " \t", cmdvector, MAX_CMD_ARG);
+			execvp(cmdvector[0], cmdvector);
+		}
+	}
+
+}
+
+
+
+			
 int main(int argc, char** argv){
 	int i = 0;
 	int chk_bg = 0;
 	int nv;
-	char cmdtemp[BUFSIZ], *chkPipe, *cmdin1, *cmdin2, *cmdout1, *cmdout2, *posIn, *posOut;
+	char cmdtemp[BUFSIZ];
 
 	signal(SIGCHLD, sigchldHandler);
 	signal(SIGINT, SIG_IGN);
@@ -121,14 +141,6 @@ int main(int argc, char** argv){
 	
 	pid_t pid;
 	while(1) {
-		chkPipe = NULL;
-		cmdin1 = NULL;
-		cmdin2 = NULL;
-		cmdout1 = NULL;
-		cmdout2 = NULL;
-		posIn = NULL;
-		posOut = NULL;
-
 		fputs(prompt, stdout);
 		fgets(cmdline, BUFSIZ, stdin);
 		
@@ -143,50 +155,10 @@ int main(int argc, char** argv){
 		}else
 			chk_bg = 0;
 		
-		posIn = strchr(cmdline, '<');
-		posOut = strchr(cmdline, '>');
 		
 		strcpy(cmdtemp, cmdline);
+		nv = makelist(cmdtemp, " \t", cmdvector, MAX_CMD_ARG);
 		
-		if(posIn != NULL){
-			strtok(cmdline, "<");
-			char *cmdin = strtok(NULL, ">");
-			if((strchr(cmdin, '|') != NULL)) {
-				cmdin1 = strtok(cmdin, "|");
-				cmdin2 = strtok(NULL, ">");
-			}else{
-				cmdin1 = strtok(cmdin, ">");
-				cmdin2 = NULL;
-			}
-
-			removeFirst(cmdin1);
-			removeFirst(cmdin2);
-		}
-
-		chkPipe = strchr(cmdline, '|');
-
-		if(posOut != NULL){
-			strtok(cmdtemp, ">");
-			char *cmdout = strtok(NULL, "<");
-			if((strchr(cmdout, '|') != NULL)) {
-				cmdout1 = strtok(cmdout, "|");
-				cmdout2 = strtok(NULL, "<");
-			}else{
-				cmdout1 = strtok(cmdout, "<");
-				cmdout2 = NULL;
-			}
-
-			removeFirst(cmdout1);
-			removeFirst(cmdout2);
-		}
-
-
-		if(posIn == NULL && posOut != NULL)
-			strcpy(cmdline, cmdtemp);
-
-		if(chkPipe == NULL){
-			nv = makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
-		}
 		if(strcmp(cmdvector[0], "exit") == 0)
 			return 0;
 
@@ -198,28 +170,7 @@ int main(int argc, char** argv){
 			case 0:
 				signal(SIGINT, SIG_DFL);
 				signal(SIGQUIT, SIG_DFL);		
-				if(posIn != NULL){
-					int fd1 = open(cmdin1, O_RDONLY, 0644);
-					dup2(fd1, 0);
-					close(fd1);
-				}
-
-				if(posOut != NULL){
-					int fd2 = open(cmdout1, O_WRONLY|O_CREAT, 0644);
-					dup2(fd2, 1);
-					close(fd2);
-				}
-
-				if(cmdin2 != NULL){
-					pipeProcess(cmdin2);
-				}
-
-				if(chkPipe == NULL) {
-					execvp(cmdvector[0], cmdvector);
-				}
-				else
-					pipeProcess(cmdline);
-
+				pipeProcess(cmdline);
 				fatal("main()");
 			case -1:
 				fatal("main()");
